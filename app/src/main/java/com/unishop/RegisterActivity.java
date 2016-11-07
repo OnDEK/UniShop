fa210efcc9d19fb80241cb02458fcf7e5332aea3
Daniel K <dkinney94@gmail.com>
2016-11-07 18:02:59 -0500

    checkpoint Register activity

--- app/src/main/java/com/unishop/RegisterActivity.java
+++ app/src/main/java/com/unishop/RegisterActivity.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -27,6 +30,9 @@ public class RegisterActivity extends Activity {
     EditText confirmPasswordEditText;
     EditText firstnameEditText;
     EditText lastnameEditText;
+    ImageView registerEmailImage;
+    ImageView registerPasswordImage;
+    ImageView registerPasswordConfirmImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +54,11 @@ public class RegisterActivity extends Activity {
         confirmPasswordEditText = (EditText) findViewById(R.id.registerPasswordConfirmEditText);
         firstnameEditText = (EditText) findViewById(R.id.firstnameEditText);
         lastnameEditText = (EditText) findViewById(R.id.lastnameEditText);
+        registerEmailImage = (ImageView) findViewById(R.id.registerEmailImage);
+        registerPasswordImage = (ImageView) findViewById(R.id.registerPasswordImage);
+        registerPasswordConfirmImage = (ImageView) findViewById(R.id.registerConfirmPasswordImage);
+
+
 
         final Button registerButton = (Button) findViewById(R.id.sendRegisterButton);
         registerEmailEditText.setOnFocusChangeListener(hideKeyboardListener);
@@ -56,6 +67,71 @@ public class RegisterActivity extends Activity {
         firstnameEditText.setOnFocusChangeListener(hideKeyboardListener);
         lastnameEditText.setOnFocusChangeListener(hideKeyboardListener);
 
+        registerEmailEditText.addTextChangedListener(new TextWatcher() {
+            String currentString;
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                currentString = registerEmailEditText.getText().toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(currentString.contains("@knights.ucf.edu")) {
+                    registerEmailImage.setImageResource(R.drawable.checkmark);
+                }else {
+                    registerEmailImage.setImageResource(R.drawable.exclamation_red);
+                }
+            }
+        });
+
+        confirmPasswordEditText.addTextChangedListener(new TextWatcher() {
+            String currentString;
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                currentString = confirmPasswordEditText.getText().toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(currentString.equals(registerPasswordEditText.getText().toString())) {
+                    registerPasswordConfirmImage.setImageResource(R.drawable.checkmark);
+                }else {
+                    registerPasswordConfirmImage.setImageResource(R.drawable.exclamation_red);
+                }
+            }
+        });
+
+        registerPasswordEditText.addTextChangedListener(new TextWatcher() {
+            String currentString;
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                currentString = registerPasswordEditText.getText().toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(currentString.contains("") {
+                    registerPasswordImage.setImageResource(R.drawable.checkmark);
+                }else {
+                    registerPasswordImage.setImageResource(R.drawable.exclamation_red);
+                }
+            }
+        });
     }
 
     public void handleRegisterRequest(View v) {
