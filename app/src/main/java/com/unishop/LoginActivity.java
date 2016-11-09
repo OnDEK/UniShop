c67c67a6ec1e1db65909e133a7d7270a707f275a
OnDEK <dkinney94@gmail.com>
2016-11-08 19:24:04 -0500

    Register password restrictions
    indicateds for good password set

--- app/src/main/java/com/unishop/LoginActivity.java
+++ app/src/main/java/com/unishop/LoginActivity.java
@@ -40,6 +40,12 @@ public class LoginActivity extends AppCompatActivity {
         userEmail.setOnFocusChangeListener(hideKeyboardListener);
         userPassword.setOnFocusChangeListener(hideKeyboardListener);
 
+        boolean TEST_MODE = true;
+        if(TEST_MODE) {
+            userEmail.setText("@knights.ucf.edu");
+            userPassword.setText("test");
+        }
+
 
     }
 
