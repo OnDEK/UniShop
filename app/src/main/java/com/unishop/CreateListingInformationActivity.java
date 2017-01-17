ccd4c8d7c34ed5b13fb5e8df39bf54e5ab3e4fb5
OnDEK <dkinney94@gmail.com>
2017-01-17 14:17:43 -0500

    New Listing UI
    created items for the upload listing UI, still need to be design but
    funtionality is done.

--- app/src/main/java/com/unishop/CreateListingInformationActivity.java
+++ app/src/main/java/com/unishop/CreateListingInformationActivity.java
@@ -1,7 +1,9 @@
 package com.unishop;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Daniel on 1/17/17.
@@ -12,5 +14,20 @@ public class CreateListingInformationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_listing_information_activity);
+
+    }
+
+    public void onBackClick(View v){
+        finish();
+    }
+
+    public void onCancelClick(View v){
+        super.finish();
+        finish();
+    }
+
+    public void onSubmitClick(View v){
+        Intent intent = new Intent(this, HomeActivity.class );
+        startActivity(intent);
     }
 }
