c8ee21f7ff9aa921cf0bf937d6ba255052571c80
OnDEK <dkinney94@gmail.com>
2017-01-19 03:43:15 -0500

    Custom List Adaptor
    created custom list to hold listings poster by the user.

--- app/src/main/java/com/unishop/HomeActivity.java
+++ app/src/main/java/com/unishop/HomeActivity.java
@@ -3,10 +3,17 @@ package com.unishop;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.roughike.bottombar.BottomBar;
@@ -16,7 +23,7 @@ import com.roughike.bottombar.OnTabSelectListener;
  * Created by Daniel on 10/30/16.
  */
 
-public class HomeActivity extends Activity{
+public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +34,9 @@ public class HomeActivity extends Activity{
         String firstname = intent.getStringExtra("firstname");
         String lastname = intent.getStringExtra("lastname");
 
-       // TextView homeTextView = (TextView) findViewById(R.id.homeScreenTextView);
-       // homeTextView.setText("Welcome back "+ firstname + "!");
-       // homeTextView.setTextColor(0xFF000000);
+        // TextView homeTextView = (TextView) findViewById(R.id.homeScreenTextView);
+        // homeTextView.setText("Welcome back "+ firstname + "!");
+        // homeTextView.setTextColor(0xFF000000);
 
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
@@ -38,7 +45,7 @@ public class HomeActivity extends Activity{
                 FragmentManager fragmentManager = getFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-                switch(tabId) {
+                switch (tabId) {
                     case R.id.bottombaritemone:
                         BidsFragment bidFragment = new BidsFragment();
                         fragmentTransaction.replace(R.id.contentContainer, bidFragment);
@@ -48,6 +55,7 @@ public class HomeActivity extends Activity{
                         ListingsFragment listingsFragment = new ListingsFragment();
                         fragmentTransaction.replace(R.id.contentContainer, listingsFragment);
                         fragmentTransaction.commit();
+
                         break;
                     case R.id.bottombaritemthree:
                         HomeFragment homeFragment = new HomeFragment();
@@ -69,9 +77,17 @@ public class HomeActivity extends Activity{
                 }
             }
         });
+
     }
+
     public void onCreateListingClick(View v) {
         Intent intent = new Intent(this, CreateListingPhotoActivity.class);
         startActivity(intent);
     }
+
+    public String[] populateString() {
+
+        return new String[0];
+    }
+
 }
--- app/src/main/java/com/unishop/ListingsFragment.java
+++ app/src/main/java/com/unishop/ListingsFragment.java
@@ -1,12 +1,18 @@
 package com.unishop;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * Created by Daniel on 1/13/17.
@@ -21,5 +27,63 @@ public class ListingsFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ListView ll = (ListView) getActivity().findViewById(R.id.listingList);
+        CustomAdapter cus = new CustomAdapter();
+        ll.setAdapter(cus);
 
+
+    }
+    String[] data_array = {"test post", "another test"};
+    public class CustomAdapter extends BaseAdapter {
+        LayoutInflater mInflater;
+
+
+        public CustomAdapter() {
+            mInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return data_array.length;//listview item count.
+        }
+
+        @Override
+        public Object getItem(int position) {
+            // TODO Auto-generated method stub
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // TODO Auto-generated method stub
+            final ViewHolder vh;
+            vh = new ViewHolder();
+
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.item_listing_personal, parent, false);
+                vh.title= (TextView)convertView.findViewById(R.id.editText4);
+                //inflate custom layour
+
+            } else {
+                convertView.setTag(vh);
+            }
+            vh.title.setText(data_array[position]);
+            return convertView;
+        }
+
+        class ViewHolder {
+            TextView title, noStarBid, oneStarBid, twoStarBid, threeStarBid, fourStarBid,fiveStarBid;
+            ImageView thumbnail;
+        }
+    }
 }
